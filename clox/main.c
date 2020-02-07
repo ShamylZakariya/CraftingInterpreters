7653c19f6285e2bbd5027c8d8a45de056418e455
Shamyl Zakariya <shamyl@gmail.com>
2020-02-07 08:15:07 -0800

    Chapter 15

--- clox/main.c
+++ clox/main.c
@@ -3,21 +3,38 @@
 #include <libclox/chunk.h>
 #include <libclox/common.h>
 #include <libclox/debug.h>
+#include <libclox/vm.h>
 
 int main(int argc, const char* argv[])
 {
-
+    initVM();
     Chunk chunk;
     initChunk(&chunk);
 
-    for (int i = 0; i < 300; i++) {
-      Value val = (i + 30.0) * 14.567;
-      writeConstant(&chunk, val, i);
-    }
+
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_ADD, 123);
+
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+    writeChunk(&chunk, OP_NEGATE, 123);
+
     writeChunk(&chunk, OP_RETURN, 123);
 
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
 
+    freeVM();
     freeChunk(&chunk);
 
     return 0;
