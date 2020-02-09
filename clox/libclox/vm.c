9d81e44d74ffaecadec551560b65bb6b1b270c4c
Shamyl Zakariya <shamyl@gmail.com>
2020-02-08 16:41:41 -0800

    Chapter 17

--- clox/libclox/vm.c
+++ clox/libclox/vm.c
@@ -112,6 +112,20 @@ Value pop()
 
 InterpretResult interpret(const char* source)
 {
-    compile(source);
-    return INTERPRET_OK;
+    Chunk chunk;
+    initChunk(&chunk);
+
+    if (!compile(source, &chunk)) {
+        freeChunk(&chunk);
+        return INTERPRET_COMPILE_ERROR;
+    }
+
+    vm.chunk = &chunk;
+    vm.ip = vm.chunk->code;
+
+    InterpretResult result = run();
+
+    freeChunk(&chunk);
+    return result;
+
 }
