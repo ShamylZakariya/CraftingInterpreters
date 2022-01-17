9b4f10f1b5ff16fd3f8a935e70bf3d3f5a7ba6e4
Shamyl Zakariya <shamyl@gmail.com>
2022-01-17 14:01:07 -0800

    clox-chap18

--- clox/libclox/value.c
+++ clox/libclox/value.c
@@ -3,6 +3,19 @@
 #include "memory.h"
 #include "value.h"
 
+bool valuesEqual(Value a, Value b)
+{
+    if (a.type != b.type) {
+        return false;
+    }
+    switch (a.type) {
+        case VAL_BOOL: return AS_BOOL(a) == AS_BOOL(b);
+        case VAL_NIL: return true;
+        case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+        default: return false; // Unreachable
+    }
+}
+
 void initValueArray(ValueArray* array)
 {
     array->values = NULL;
