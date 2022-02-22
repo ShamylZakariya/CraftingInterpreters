dc8830dec2a1a242719f6fe7d6380b58e870324b
Shamyl Zakariya <shamyl@gmail.com>
2022-02-22 07:14:14 -0800

    Fixes to clox-25.1

--- clox/libclox/object.c
+++ clox/libclox/object.c
@@ -108,6 +108,7 @@ void printObject(Value value)
     switch (OBJ_TYPE(value)) {
     case OBJ_CLOSURE:
         printFunction(AS_CLOSURE(value)->function);
+        break;
     case OBJ_FUNCTION:
         printFunction(AS_FUNCTION(value));
         break;
