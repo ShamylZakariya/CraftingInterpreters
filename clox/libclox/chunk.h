9a045a07aec08178983435b967ff0473d822743e
Shamyl Zakariya <shamyl@gmail.com>
2022-03-16 20:26:48 -0700

    clox-chap29.3

--- clox/libclox/chunk.h
+++ clox/libclox/chunk.h
@@ -19,6 +19,7 @@ typedef enum OpCode {
     OP_SET_UPVALUE,
     OP_GET_PROPERTY,
     OP_SET_PROPERTY,
+    OP_GET_SUPER,
     OP_EQUAL,
     OP_GREATER,
     OP_LESS,
@@ -38,6 +39,7 @@ typedef enum OpCode {
     OP_CLOSE_UPVALUE,
     OP_RETURN,
     OP_CLASS,
+    OP_INHERIT,
     OP_METHOD,
 } OpCode;
 
